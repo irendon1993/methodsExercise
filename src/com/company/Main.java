5663f1c1d8b1efbbebdb4956778811308da343b3
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-12-15 15:01:26 -0500

    used methods to solve challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        calculateScore(true, 800 , 5, 100);
+
+        calculateScore(true,10000, 8, 200);
+
+    }
+
+    public static void calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("your final score was " + finalScore);
+        }
     }
 }

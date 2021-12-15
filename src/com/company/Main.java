f2473beeb98015cb8e1081201e19d938e7915633
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-12-15 15:23:18 -0500

    creating calculateScore algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800 , 5, 100);
+        boolean gameOver = true;
+        int score =800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true,10000, 8, 200);
+        calculateScore(gameOver,score,levelCompleted,bonus);
+
+        score = 10000;
+        levelCompleted= 8;
+        bonus = 200;
+
+        calculateScore(gameOver,score,levelCompleted,bonus);
+
 
     }
 
-    public static void calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("your final score was " + finalScore);
+            System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }
+            return -1;
         }
     }
-}
